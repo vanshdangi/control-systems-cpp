4ad8c36f5f1ab6f32a1d0bcc8fba110c7127ffed
Gr8V <vanshdangi06@gmail.com>
2026-07-22 21:48:37 +0530

    added low pass filter

---
+++ include/control_systems/filters/low_pass_filter.hpp
@@ -0,0 +1,12 @@
+class LowPassFilter {
+public:
+    explicit LowPassFilter(double alpha = 0.9);
+
+    double update(double input);
+    void reset();
+
+private:
+    double alpha_;
+    double previousOutput_ = 0.0;
+    bool initialized_ = false;
+};
---
+++ src/filters/low_pass_filter.cpp
@@ -0,0 +1,26 @@
+#include <filters/low_pass_filter.hpp>
+
+LowPassFilter::LowPassFilter(double alpha
+                        ): alpha_(alpha)
+{}
+
+double LowPassFilter::update(double input)
+{
+    if (!initialized_)
+    {
+        previousOutput_ = input;
+        initialized_ = true;
+        return input;
+    }
+
+    previousOutput_ =
+        alpha_ * previousOutput_ +
+        (1.0 - alpha_) * input;
+
+    return previousOutput_;
+}
+
+void LowPassFilter::reset(){
+    previousOutput_ = 0.0;
+    initialized_ = false;
+}
